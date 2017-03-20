e3c06129d2ee3783001b945842001da69b8ceb52
hyayi <hyayi@us.ibm.com>
2017-03-19 20:14:59 -0400

    schema changes

--- server/src/users/schema.js
+++ server/src/users/schema.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 const schema = Joi.object().keys({
   firstName: Joi.string().max(40).required(),
-  lastName: Joi.string().max(20).required(),
-  address: Joi.string().max(100).required(),
+  lastName: Joi.string().max(30).required(),
+  address: Joi.string().max(150).required(),
   company: Joi.string().max(50).required(),
 });
 
